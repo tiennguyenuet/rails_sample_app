103a868349625be2d455dda9c099c433b13d84fa
Tien Nguyen <tiennguyenuet@gmail.com>
2016-06-20 09:26:58 +0700

    push heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sampleapp-tiennguyen.herokuapp.com'
+  host = 'sampleapp-rails.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 
 end
